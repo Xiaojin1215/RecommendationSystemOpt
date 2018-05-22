1a0545eb6513dff0356c0cfdd15877bfd707deac
Xiaojin Liu <xiaojinliumail@gmail.com>
2018-05-22 19:42:59 -0400

    Update the directory structure and slides

---
+++ src/business_data_process.py
@@ -0,0 +1,25 @@
+import re
+import sys
+input_path = "../input/business.json"
+output_path = "../tmp/business_name_id_index"
+
+def main():
+    business_index = 0
+    business_dict = {}
+    with open(output_path, 'w') as f_w:
+    with open(input_path, 'r') as f_r:
+        for line in f_r:
+            segs = line.strip()
+            p1 = 'business_id": "(.*?)".*?name": "(.*?)"'
+            pattern1 = re.compile(p1, re.S)
+            information = pattern1.findall(segs)
+            business_id = information[0][0]
+            business_name = information[0][1]
+            if not business_dict.has_key(business_id):
+                business_dict[str(business_id)] = business_index
+                business_index = business_index + 1
+            f_w.write(business_id+','+str(business_dict.get(business_id))+','+business_name)
+    f_w.close()
+    f_r.close()
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ src/data_simulation/item_data_gen.py
@@ -0,0 +1,24 @@
+import sys
+import re
+import random
+import string
+
+path_review = "../../tmp/small_review"
+
+last_item = 174566
+item_length = last_item + 1
+gen_rate = 4
+min_star = 1
+max_star = 5
+
+user_review = {}
+with open(path_review, 'r') as fh2:
+    for line in fh2:
+        segs = line.strip().split(',')
+        user_id = int(segs[0])
+        item_id = int(segs[1])
+        for i in range(1,gen_rate):
+            new_item = i * item_length + item_id
+            new_star = random.sample(range(min_star, max_star + 1), 1)
+            new_star = new_star[0]
+            print str(user_id) + "," + str(new_item) + "," + str(new_star)
